96e21d2b965021eabd2d7eb5dc9d27e828f03e3b
papushman <papushman@yandex.ru>
2023-12-25 21:20:33 +0300

    Initial Commit

---
+++ taskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskList
+//
+//  Created by MAC on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
